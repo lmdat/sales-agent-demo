778902513d9959265beac6d7e14a4bab568eded6
Dat Le <minh_dat_le@yahoo.com>
2025-11-13 18:02:32 +0700

    Change gemma 9b --> gpt oss 20b

--- sales-agent-backend/src/config.py
+++ sales-agent-backend/src/config.py
@@ -11,24 +11,24 @@ LLM_MODELS = {
         "router_node": os.getenv('GROQ_LLM_MODEL_LLAMA_70B')
     },
     "greeting_subgraph": {
-        "greeting_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B')
+        "greeting_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B')
     },
     "company_info_subgraph": {
-        "company_info_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B')
+        "company_info_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B')
     },
     "off_topic_subgraph": {
-        "off_topic_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B')
+        "off_topic_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B')
     },
     "product_qna_subgraph": {
         "analyze_product_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_120B'),
-        "product_qna_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B')
+        "product_qna_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B')
     },
     "make_order_subgraph": {
         "analyze_order_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_120B'),
         "inform_items_not_in_stock": os.getenv('GROQ_LLM_MODEL_LLAMA_70B'),
-        "collect_order_info_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B'),
-        "order_confirmation_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B'),
-        "create_order_summary_node": os.getenv('GROQ_LLM_MODEL_GEMMA2_9B')
+        "collect_order_info_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B'),
+        "order_confirmation_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B'),
+        "create_order_summary_node": os.getenv('GROQ_LLM_MODEL_OPENAI_OSS_20B')
     },
     "exit_subgraph": {
         "exit_node": os.getenv('GROQ_LLM_MODEL_LLAMA_70B')
